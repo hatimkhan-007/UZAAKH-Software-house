9f60f736f4b6bcad268be4dce388377125491cea
Uzair Khan <uzairkhan60621@gmail.com>
2026-07-29 11:29:29 -0700

    Updation of About.jsx

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div>About
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate sequi rerum vitae, optio iusto quod, facere eveniet eius rem excepturi in dolorum unde perspiciatis officiis recusandae eaque. Quis, ut iusto?</p>
+    </div>
   )
 }
 
